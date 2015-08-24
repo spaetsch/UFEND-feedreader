6e162e5e84231bb04bc23f8cafdbd7452f0a186a
Sarah Paetsch <spaetsch@gmail.com>
2015-08-24 14:50:40 -0700

    First two suites work

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,33 +27,58 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* This test loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('each has URL', function() {
+          for (i=0; i < allFeeds.length; i++){
+            expect(allFeeds[i].url).toBeDefined();
+            expect(allFeeds[i].url.length).not.toBe(0);
+          }
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('each has name', function() {
+          for (i=0; i < allFeeds.length; i++){
+            expect(allFeeds[i].name).toBeDefined();
+            expect(allFeeds[i].name.length).not.toBe(0);
+          }
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         /*verify body has class menu-hidden*/
+        it('hidden by default', function() {
+          expect($("body").attr("class")).toBe("menu-hidden");
+        });
 
-         /* TODO: Write a test that ensures the menu changes
+         /* Test ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('visible on click', function() {
+          //does the menu display when clicked
+          $('a.menu-icon-link').click();
+          expect($("body").attr("class")).not.toBe("menu-hidden");
+          //does it hide when clicked again
+          $('a.menu-icon-link').click();
+          expect($("body").attr("class")).toBe("menu-hidden");
+        });
 
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
